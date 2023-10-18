8ce2ff1af6ecce9a1098489ca99543e6816320f7
hau_v <hau_vu94@hotmail.com>
2023-10-18 13:21:24 +0200

    oppdatert Bolig klassen, lagt til nye fields, oppdatert constructor, laget nye getters. Laget 2 metoder, en for å lagre bud, en for vinnerbud når bolig er solgt Også oppdatert fjerde check på Funksjonalitetskrav 1

--- src/main/java/org/example/Bolig.java
+++ src/main/java/org/example/Bolig.java
@@ -1,18 +1,33 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bolig {
     //fields
     private String gateAdresse;
     private String postNummer;
     private int takstPris;
 
+    private Bud vinnerBud;
+    private List<Bud> budList;
+
     // constructor
     public Bolig(String gateAdresse, String postNummer, int takstPris) {
         this.gateAdresse = gateAdresse;
         this.postNummer = postNummer;
         this.takstPris = takstPris;
+        this.budList = new ArrayList<>();
     }
 
+    // methods
+    public void leggTilBud(Bud bud){
+        budList.add(bud);
+    }
+
+    public void solgt(Bud vinnerBud){
+        this.vinnerBud = vinnerBud;
+    }
 
     // getters
 
@@ -26,5 +41,13 @@ public class Bolig {
 
     public int getTakstPris() {
         return takstPris;
+    }
+
+    public Bud getVinnerBud() {
+        return vinnerBud;
+    }
+
+    public List<Bud> getBudList() {
+        return budList;
     }
 }
